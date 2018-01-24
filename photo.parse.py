1db0922449b8f95e73eb2d788c02f50172fd5d69
Annabelle Strong <anstrong@users.noreply.github.com>
2018-01-24 17:01:17 -0500

    Uploaded version 1 of insta parse

--- photo.parse.py
+++ photo.parse.py
@@ -1,164 +1,52 @@
-from urllib.request import Request, urlopen
+import urllib.request as urllib
 from bs4 import BeautifulSoup, SoupStrainer
-import time
+import os
 import subprocess
-
-# ASK FOR:
-	# url
-	# number of photos (from top)
-	## pick frame
-# Loop through home page JS
-# Find each image's largest url
-# Download... probably to Dropbox folder
-# Have IFTTT send new photos via email to frame
-
-# GUI ideas
-	# Select exact photos
-		# Thumbnail grid?
-	# Folder selector thingie
-	# Option to pick date range instead of specific photos
-
-# Add-ons
-	# Hashtag filter
-	# Tagged filter
+import re
 
 ####################################### FUNCTIONS ###############################################
-
-def read_html(url):
+def read_html(address):
 	# Open and parse the page
-	html = urlopen(url).read()
+	url = urllib.Request(address, headers={'User-Agent': 'Mozilla/5.0'})
+	html = urllib.urlopen(url).read()
 
 	return html
 
-def get_url(y):
-	# Add end 'index' code if code not included
-	h = y.rfind('.html')
-
-	if h == -1:
-		y = str(y + 'index.html')
-
-	# Isolate 'code' (page identifier) for processing
-	f = y.rfind('_')
-	g = y.rfind('/')
-
-	if f == -1:
-		start1 = y.rfind('/') + 1
-		end1 = y.find('.html')
-		code = y[start1:end1]
-
-	elif f !=-1 and (f < g):
-		start1 = y.rfind('/') + 1
-		end1 = y.find('.html')
-		code = y[start1:end1]
-
-	else:
-		start1 = y.rfind('/') + 1
-		end1 = y.rfind('_')
-		code = y[start1:end1]
-
-	# Identify 'path' (rest of url) using code
-	start = y.find('www.')
-	end = y.find(str(code))
-	path = y[start:end]
-
-	return path, code
-
-def get_title(html):
-	# Find Title element
-	title_text = SoupStrainer("title")
-	title_soup = str(BeautifulSoup(html, "html.parser", parse_only=title_text))
-
-	# Isolate actual title
-	title_start = title_soup.find('Read ') + 5
-	title_end = title_soup.find(' online')
-	title = title_soup[title_start:title_end]
-
-	return title
-
-def get_pages(html):
-	# Find text in which the number of pages is wrapped
-	page_text = SoupStrainer("ul", {'class':'pagelist'})
-	page_soup = str(BeautifulSoup(html, "html.parser", parse_only=page_text))
-
-	# Isolate number of pages
-	pages_start = page_soup.find('<ul class="pagelist"><li><a>') + 28
-	pages_end = page_soup.find('pages:')
-	pages = page_soup[pages_start:pages_end]
-
-	return pages
-
-def make_file(name):
-	# Create filename based on book title and directory
-	title = str(name)
-	path = '/Users/annabelle_strong/Documents/Bin/Extracted Texts/'
-	filename = str(path + title + ".txt")
-
-	# 'Open' (or create if nonexistent) file
-	file = open(filename,"w")
-
-	return file, filename
-
-
 ########################################## LOGIC ###############################################
-
-# Get full url
-address = input("What's the url of the book you want to parse? ")
-
-# Split url for page looping and processing
-raw_url = get_url(address)
-path = raw_url[0]
-code = raw_url[1]
-
-# Load webpage with full url
-url = Request('http://' + str(path) + str(code) +'.html', headers={'User-Agent': 'Mozilla/5.0'})
+#address = 'http://www.instagram.com/pretty._landscapes'
+#address = 'https://www.instagram.com/laurkendall6/'
+address = 'https://www.instagram.com/puppysphere/'
 
 # Parse page
-setup_html = read_html(url)
+html = read_html(url)
 
-# Get name of book
-title = get_title(setup_html)
+soup = BeautifulSoup(html, "html.parser")
 
-# Create .txt file
-txt = make_file(title)
-file = txt[0]
-filename = str(txt[1])
+text = 'Script: '
 
-# Get number of pages
-pages = int(get_pages(setup_html))
+for script in soup(["script"]):
+    text += script.get_text()
 
+indicator = '''"config_width": 640, "config_height": 640}], "is_video": false,''' # "code":"BNPjw9JAyuy", "date": 1480098330, "display_src":
 
-# Loop through pages
-for a in range(1, pages+1):
-	if a != 1:
-		# Load new url based on page number
-		new_url = Request('http://' + str(path) + str(code) + '_' + str(a) +'.html', headers={'User-Agent': 'Mozilla/5.0'})
-		# Read updated html
-		html = read_html(new_url)
-	else:
-		html = setup_html
+photos = text.split(indicator)
 
-	# Filter paragraph text and parse page
-	p = SoupStrainer("p")
+photo_paths = []
 
-	soup = BeautifulSoup(html, "html.parser", parse_only=p)
+for x in range(len(photos)):
+	if x != 0:
+		text = photos[x]
 
-	# Remove all script and style elements
-	for script in soup(["script", "style"]):
-	    script.extract()
+		start = text.find('https://')
+		end = text.find('jpg') + 3
+		photo_path = text[start:end]
 
-	# Remove copyright line
-	for p in soup.find_all("p", {'class':'info'}):
-	    p.decompose()
+		photo_paths.append(photo_path)
 
-	# Extract text
-	text = soup.get_text()
-
-	# Write text to document
-	file.write(text)
+for i in range(len(photo_paths)):
+	path = photo_paths[i]
+	urllib.urlretrieve(path, os.path.basename("Photo_" + str(i+1)))
 
 
 # Print confirmation
-print("Photo extraction complete.")
-
-# Open file location
-subprocess.call(["open", "-R", filename])
+print("Script extraction complete.")
