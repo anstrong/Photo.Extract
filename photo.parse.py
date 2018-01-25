1a18cb5aafadba36d37c58007ca1328e89199703
Annabelle Strong <annabellestrong@gmail.com>
2018-01-25 15:26:54 -0500

    Fine tuning

--- photo.parse.py
+++ photo.parse.py
@@ -8,23 +8,34 @@ import re
 
 def read_html(address):
 	# Open the page
-	url = urllib.Request(address, headers={'User-Agent': 'Mozilla/5.0'})
+	url = urllib.Request(address)#, headers={'User-Agent': 'Mozilla/5.0'})
 	# Parse the HTML
 	html = urllib.urlopen(url).read()
 
 	return html
 
+def get_account_name(address):
+	# Isolate account name
+	start = address.find('instagram.com/') + 14
+	end = len(address) - 1
+	name = address[start:end]
+
+	return name
+
 ########################################## LOGIC ###############################################
 
 #address = 'http://www.instagram.com/pretty._landscapes'
 #address = 'https://www.instagram.com/laurkendall6/'
-address = 'https://www.instagram.com/puppysphere/'
+#address = 'https://www.instagram.com/puppysphere/'
+address = 'https://www.instagram.com/universal.puppies/'
 
 # Open and parse page
-html = read_html(url)
+html = read_html(address)
 
 # Turn raw html into scrapable 'soup'
 soup = BeautifulSoup(html, "html.parser")
+
+text = ''
 
 # Extract all text with 'script' tag
 for script in soup(["script"]):
@@ -48,13 +59,23 @@ for x in range(len(photos)):
 
 		photo_paths.append(photo_path)	# Add url to photo path array
 
+# Get name for folder from account name
+folder = get_account_name(address)
+file_path = '/Users/annabelle_strong/Documents/Bin/Extracted Images/' + folder + '/'
+
+try:
+	os.chdir(file_path)
+except FileNotFoundError:
+	os.mkdir(file_path)
+	os.chdir(file_path)
+	
 # Cycle through photo urls and dowload the images
 for i in range(len(photo_paths)):
 	path = photo_paths[i]
-	urllib.urlretrieve(path, os.path.basename("Photo_" + str(i+1)))
+	urllib.urlretrieve(path, os.path.basename('Photo_%(num)s.jpg' %{'num': str(i+1)}))
 
 # Print confirmation
-print("Script extraction complete.")
+print("Image extraction complete.")
 
 # Open folder
-subprocess.call(["open", "-R", "Photo_1"]])
+subprocess.call(["open", "-R", file_path])
